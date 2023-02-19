e33eef4d319b9e19a823c329a37ec294a061bfe1
Koustav Muhuri <soutickmuhuri@gmail.com>
2023-02-19 12:16:11 +0530

    Employee Management backend

---
+++ src/main/java/com/example/demo/GithubSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSpringProjectApplication.class, args);
+	}
+
+}
